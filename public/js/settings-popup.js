cebdeadae9623affbb16a2bda8aeb689c0f2b85f
Sa Sa <ssalihsrz@gmail.com>
2025-10-30 16:05:41 -0400

    v3.1.183 - Fix empty sessions bug

--- public/js/settings-popup.js
+++ public/js/settings-popup.js
@@ -413,7 +413,7 @@ async function checkForUpdates() {
         const data = await response.json();
         
         const latestVersion = data.tag_name.replace('v', '');
-        const currentVersion = '3.1.182';
+        const currentVersion = '3.1.183';
         
         if (button) {
             button.innerHTML = '<i class="fa-solid fa-check"></i> Check Complete';
