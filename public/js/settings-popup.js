2ac1d15a902be58398e640c7535c09ceb4922cc6
Sa Sa <ssalihsrz@gmail.com>
2025-10-30 15:07:26 -0400

    v3.1.180 - Fix auto-updater notifications

--- public/js/settings-popup.js
+++ public/js/settings-popup.js
@@ -400,7 +400,7 @@ async function checkForUpdates() {
         const data = await response.json();
         
         const latestVersion = data.tag_name.replace('v', '');
-        const currentVersion = '3.1.179';
+        const currentVersion = '3.1.180';
         
         if (button) {
             button.innerHTML = '<i class="fa-solid fa-check"></i> Check Complete';
